6b2d7a9dcea82fd888ffcd027622ae4ca747fb1a
mcharo <mcharoprofg23@gmail.com>
2023-05-15 10:31:07 +0300

    adding some features of dialogs

--- app/src/main/java/wito/government/app/activities/ItemViewer.java
+++ app/src/main/java/wito/government/app/activities/ItemViewer.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -30,12 +32,17 @@ import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import wito.government.app.Home;
 import wito.government.app.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Locale;
 
 public class ItemViewer extends AppCompatActivity {
 
     SimpleExoPlayer player;
     ProgressBar progressBar;
+
+    private final Calendar calendar = Calendar.getInstance();
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -109,19 +116,87 @@ public class ItemViewer extends AppCompatActivity {
             }
         });
 
-        View.OnClickListener listener = view -> {
-            // OPENING DIALOG
-            NunuaHisa();
-        };
+        nunuaHisaBtn.setOnClickListener(v -> NunuaHisa());
+        uzaHisaBtn.setOnClickListener(v -> UzaHisa());
+        miadiBtn.setOnClickListener(v -> MiadiYaKazi());
+    }
 
-        nunuaHisaBtn.setOnClickListener(listener);
-        uzaHisaBtn.setOnClickListener(listener);
-        miadiBtn.setOnClickListener(listener);
+    private void MiadiYaKazi() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(ItemViewer.this);
+
+        View view = getLayoutInflater().inflate(R.layout.dialog_miadi_ya_kazi, null);
+        builder.setView(view);
+        TextView textStartCal = view.findViewById(R.id.textStartCal);
+        TextView textEndtCal = view.findViewById(R.id.textEndtCal);
+        EditText edit_uthibitisho_wa_miadi = view.findViewById(R.id.edit_uthibitisho_wa_miadi);
+        TextView cancel_button = view.findViewById(R.id.cancel_button);
+
+        DateChooser(textStartCal);
+        DateChooser(textEndtCal);
+
+        Button btn_miadi_ya_kazi = view.findViewById(R.id.btn_miadi_ya_kazi);
+
+        final AlertDialog dialog = builder.create();
+
+        btn_miadi_ya_kazi.setOnClickListener(view1 -> {
+            if (textStartCal.getText().toString().isEmpty()) {
+                textStartCal.setError("Tafadhali chagua tarehe!");
+                textStartCal.requestFocus();
+            } else if (textEndtCal.getText().toString().trim().isEmpty()) {
+                textEndtCal.setError("Tafadhali chagua tarehe!");
+                textEndtCal.requestFocus();
+            } else if (edit_uthibitisho_wa_miadi.getText().toString().trim().isEmpty()) {
+                edit_uthibitisho_wa_miadi.setError("Tafadhali weka uthibitisho!");
+                edit_uthibitisho_wa_miadi.requestFocus();
+            } else {
+                dialog.dismiss();
+                Toast.makeText(ItemViewer.this, "Miadi ya kazi", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cancel_button.setOnClickListener(view12 -> dialog.dismiss());
+        dialog.show();
+    }
+
+    private void UzaHisa() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(ItemViewer.this);
+
+        View view = getLayoutInflater().inflate(R.layout.dialog_uza_hisa, null);
+        builder.setView(view);
+        EditText edt_kiasi = view.findViewById(R.id.edt_kiasi);
+        EditText edt_neno_siri = view.findViewById(R.id.edt_neno_siri);
+        EditText edt_rudia_neno_siri = view.findViewById(R.id.edt_rudia_neno_siri);
+        TextView cancel_button = view.findViewById(R.id.cancel_button);
+
+        Button btn_uza_hisa = view.findViewById(R.id.btn_uza_hisa);
+
+        final AlertDialog dialog = builder.create();
+
+        btn_uza_hisa.setOnClickListener(view1 -> {
+            if (edt_kiasi.getText().toString().isEmpty()) {
+                edt_kiasi.setError("Tafadhali weka kiasi!");
+                edt_kiasi.requestFocus();
+            } else if (edt_neno_siri.getText().toString().trim().isEmpty()) {
+                edt_neno_siri.setError("Tafadhali ingiza neno siri!");
+                edt_neno_siri.requestFocus();
+            } else if (edt_rudia_neno_siri.getText().toString().trim().isEmpty()) {
+                edt_rudia_neno_siri.setError("Tafadhali rudia neno siri!");
+                edt_rudia_neno_siri.requestFocus();
+            } else if (!edt_neno_siri.getText().toString().trim().equals(edt_rudia_neno_siri.getText().toString().trim())) {
+                edt_rudia_neno_siri.setError("Neno siri halifanani");
+                edt_rudia_neno_siri.requestFocus();
+            } else {
+                dialog.dismiss();
+                Toast.makeText(ItemViewer.this, "Uza hisa", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cancel_button.setOnClickListener(view12 -> dialog.dismiss());
+        dialog.show();
     }
 
     private void NunuaHisa() {
         AlertDialog.Builder builder = new AlertDialog.Builder(ItemViewer.this);
-        builder.setTitle("Nunua hisa");
 
         View view = getLayoutInflater().inflate(R.layout.dialog_nunua_hisa, null);
         builder.setView(view);
@@ -148,12 +223,33 @@ public class ItemViewer extends AppCompatActivity {
                 edt_rudia_neno_siri.setError("Neno siri halifanani");
                 edt_rudia_neno_siri.requestFocus();
             } else {
-                Toast.makeText(ItemViewer.this, "Hello", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+                Toast.makeText(ItemViewer.this, "Nunua hisa", Toast.LENGTH_SHORT).show();
             }
         });
 
         cancel_button.setOnClickListener(view12 -> dialog.dismiss());
         dialog.show();
+    }
+
+    private void DateChooser(TextView textView) {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+            private void updateLabel() {
+                //date format yyyy-MM-dd
+                String format = "yyyy-MM-dd";
+                SimpleDateFormat dateFormat = new SimpleDateFormat(format, Locale.US);
+                textView.setText(dateFormat.format(calendar.getTime()));
+            }
+        };
+        textView.setOnClickListener(v -> new DatePickerDialog(ItemViewer.this, dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show());
     }
 
     @Override
